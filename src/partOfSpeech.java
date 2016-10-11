33e13cf34daf9ced1881c570525554be5b4a74b8
17isenseed <17isenseed@CTE0615810166.kisdtx.com>
2016-10-11 11:45:11 -0500

    solved partOfSpeech

--- src/partOfSpeech.java
+++ src/partOfSpeech.java
@@ -5,37 +5,31 @@ public class partOfSpeech {
 	public static void main(String[] args) throws IOException {
 		File inFile = new File ("partOfSpeech.dat");
 		Scanner scan = new Scanner(inFile); //the partOfSpeech text
-		
 		String[] nounWords = scan.nextLine().split(" ");
-		
 		String verbWordsInit = scan.nextLine() + " " + scan.nextLine();
 		String[] verbWords = verbWordsInit.split(" ");
-		
 		int dataSets = Integer.parseInt(scan.nextLine());
-		
 		for (int i = 0; i < dataSets; i++) { //work for a set
 			String[] toSearch = scan.nextLine().toLowerCase().split(" ");
 			int indivSets = Integer.parseInt(toSearch[1]);
 			int indexOfSearch = 0;
-			
 			for (int j = 0; j < indivSets; j++) {
-				System.out.println(j);
 				String currString[] = scan.nextLine().split(" ");
-				System.out.println("string to find: " + toSearch[0]);
-				for (int a = 0; a < currString.length-1; a++) { //finds where string to search for is
+				currString[currString.length-1] = currString[currString.length-1].replace(".", "");
+				currString[currString.length-1] = currString[currString.length-1].replace("!", "");
+				currString[currString.length-1] = currString[currString.length-1].replace("?", "");
+				for (int a = 0; a < currString.length; a++) { //finds where string to search for is
 					if (currString[a].equals(toSearch[0])) {
 						indexOfSearch = a;
 					}
-					System.out.println(currString[a]);
 				}
-				System.out.println(currString[indexOfSearch] + currString[indexOfSearch-1]);
 				for (int e = 0; e < nounWords.length-1; e++) {
-					if (currString[indexOfSearch-1].equals(nounWords[e])) {
+					if (currString[indexOfSearch-1].equalsIgnoreCase(nounWords[e])) {
 						System.out.println("NOUN");
 					}
 				}
 				for (int e = 0; e < verbWords.length-1; e++) {
-					if (currString[indexOfSearch-1].equals(verbWords[e])) {
+					if (currString[indexOfSearch-1].equalsIgnoreCase(verbWords[e])) {
 						System.out.println("VERB");
 					}
 				}
