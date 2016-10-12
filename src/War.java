f88c895db24d8969e72289547316dd6b02e685db
Isensee <17isenseed@kisdtx.com>
2016-10-12 14:25:27 -0500

    continued working on War

--- src/War.java
+++ src/War.java
@@ -3,6 +3,12 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class War {
+	//int[] set = [2];
+	
+	public boolean val(int card1, int card2) {
+		return false;
+	}
+	
 	public static void main(String[] args) throws IOException {
 		Scanner in = new Scanner(new File("war.dat"));
 		int sets = Integer.parseInt(in.nextLine());
