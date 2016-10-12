7f67368648b5732ec3c942f04dc27586a20378dc
Derek Isensee - from school <derek.isensee@gmail.com>
2016-10-12 09:07:38 -0500

    Revert "Revert "st""
    This reverts commit 3d28ee52088ddc00d27c322ee635ee9eb1162c8d.

---
+++ src/War.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class War {
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(new File("war.dat"));
+		int sets = Integer.parseInt(in.nextLine());
+		
+		while (sets-- > 0) {
+			
+		}
+	}
+
+}
