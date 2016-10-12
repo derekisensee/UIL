be3ee4e754a0fdfb25bcb4b4799458e2752fcf71
Derek Isensee <derek.isensee@gmail.com>
2016-10-12 15:55:40 -0500

    continued War

--- src/War.java
+++ src/War.java
@@ -3,9 +3,17 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class War {
-	//int[] set = [2];
+	int[] arr = new int[11];
 	
-	public boolean val(int card1, int card2) {
+	public boolean val(String cardOne, String cardTwo) { //figure out what to return
+		if (cardOne.equals("J") || cardTwo.equals("J") || cardOne.equals("Q") || cardTwo.equals("Q")|| cardOne.equals("K") || cardTwo.equals("K") || cardOne.equals("A") || cardTwo.equals("A")) {
+			
+		}
+		else {
+			if (Integer.parseInt(cardOne) > Integer.parseInt(cardTwo)) {
+				
+			}
+		}
 		return false;
 	}
 	
@@ -14,8 +22,12 @@ public class War {
 		int sets = Integer.parseInt(in.nextLine());
 		
 		while (sets-- > 0) {
+			String[] deckOne = in.nextLine().split(" ");
+			String[] deckTwo = in.nextLine().split(" ");
+			
 			
 		}
+		in.close();
 	}
 
 }
