9ee2a53b97da46e908f1268fd82cebc80ce5b7ca
Isensee <17isenseed@kisdtx.com>
2016-10-28 14:14:27 -0500

    Puddle Bop update

--- src/Puddle.java
+++ src/Puddle.java
@@ -4,13 +4,35 @@
 import java.util.Scanner;
 import java.io.*;
 
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Int;
+
 public class Puddle {
+    public static String bop(String[][] m, int[] c) {
+        if(m[c[0]][c[1]].equals("G"))
+            return "OK";
+        else
+            return "NOT OK";
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new File("puddle.dat"));
-        int size = Integer.parseInt(in.nextLine());
 
-        while(size-- > 0) {
-            System.out.println();
+        String[] gameBoard = in.nextLine().split(" ");
+        int r = Integer.parseInt(gameBoard[1]); int c = Integer.parseInt(gameBoard[0]); //rows and columns
+        String[][] matrix = new String[r][c];
+
+        for(int i = 0; i < r; i++) { //matrix creation
+            String[] row = in.nextLine().split("");
+            for (int j = 0; j < row.length; j++) {
+                matrix[i][j] = row[j];
+            }
+        }
+        //////////////////
+        int sets = Integer.parseInt(in.nextLine());
+        while(sets-- > 0) {
+            String[] s = in.nextLine().split(" ");
+            int[] cord = new int[] {Integer.parseInt(s[0]), Integer.parseInt(s[1])};
+            System.out.println(bop(matrix, cord));
         }
     }
 }
