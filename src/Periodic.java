616d2bef8e59fb43641f89349c5eb27aacb3d1a8
Derek Isensee <derek.isensee@gmail.com>
2016-11-01 09:05:37 -0500

    Update Periodic

--- src/Periodic.java
+++ src/Periodic.java
@@ -15,13 +15,37 @@ public class Periodic {
         Collections.sort(table);
         ArrayList<String> tableSingle = new ArrayList();
         ArrayList<String> tableDouble = new ArrayList();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++) { //separates table into a single-lettered table and a double-lettered table
             if (table.get(i).length() == 1)
                 tableSingle.add(table.get(i));
             else
                 tableDouble.add(table.get(i));
         }
-        System.out.println(tableSingle);
-        System.out.println(tableDouble);
+        in.nextLine();
+        int sets = Integer.parseInt(in.nextLine());
+        while (sets-- > 0) {
+            String[] word = in.nextLine().split("");
+            for (int i = 0; i < word.length; i++) {
+                for (int j = 0; j < tableSingle.size(); j++) {
+                    if (tableSingle.get(j).equalsIgnoreCase(word[i])) {
+                        System.out.println(tableSingle.get(j) + " " + word[i]);
+                        break;
+                    }
+                    else {
+                        for (int k = 0; k < tableDouble.size(); k++) {
+                            //if (i != word.length-1)
+                            String comp = word[i] + word[i + 1]; //because i isn't being incremented, the same stuff is being constantly compared
+                            System.out.println(comp);
+                            if (i != word.length - 1 && tableDouble.get(k).equalsIgnoreCase(word[i] + word[i + 1])) {
+                                System.out.println("------------\nCOMPARED WORDS:\n" + tableDouble.get(k) + " AND " + word[i]+word[i+1] + " " + k + "\n---------------");
+                                System.out.println(tableDouble.get(k) + " " + word[i] + word[i + 1]);
+                                break; //this doensn't seem to be working
+                            }
+                        }
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 }
