1e49213a5b1b3ede45839005accf5e9498eeb3fd
Derek Isensee <derek.isensee@gmail.com>
2016-12-07 09:04:52 -0600

    betterDara update

--- src/betterDara.java
+++ src/betterDara.java
@@ -4,12 +4,38 @@
 import java.util.*;
 import java.io.*;
 public class betterDara {
+    static String termDeriv(String p) {
+        System.out.println(p);
+        return "";
+    }
+
     static String derivOf(String p) {
         String deriv = "";
-        String[] splitUp = p.split("[\\+|\\-]");
-        for (int i = 0; i < splitUp.length; i++) {
-            System.out.println(splitUp[i]);
+        System.out.println("orig: " + p);
+        String[] polySplit = p.split("\\s\\+|\\s-");
+        String[] signSplit = p.split("[^+|-]+");
+
+        if (polySplit.length > 1) {
+            if (signSplit[0].equals("-")) {
+                for (int i = 0; i < polySplit.length; i++) {
+                    if (i == 0)
+                        System.out.print(polySplit[i]);
+                    else
+                        System.out.print(signSplit[i] + polySplit[i]);
+                }
+            }
+            else
+                for (int i = 0; i < polySplit.length; i++) {
+                    System.out.print(signSplit[i] + polySplit[i]);
+                }
         }
+        else {
+            for (int i = 0; i < polySplit.length; i++) {
+                System.out.print(polySplit[i]);
+            }
+        }
+        System.out.println();
+        System.out.println();
         return deriv;
     }
 
@@ -17,7 +43,8 @@ public class betterDara {
         Scanner in = new Scanner(new File("dara.dat"));
         while(in.hasNext()) {
             String poly = in.nextLine();
-            System.out.println(derivOf(poly) + " : " + derivOf(derivOf(poly)));
+            derivOf(poly.trim());
+            //System.out.println(derivOf(poly) + " : " + derivOf(derivOf(poly)));
         }
     }
 }
